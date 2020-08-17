2a81b1d6b806acf60dd79e1a9254365cd7334968
kujebe <kujebeseun@gmail.com>
2020-08-17 09:46:25 +0100

    Implemented scopus retrieval from query string

--- src/lib/api/scopusApi.js
+++ src/lib/api/scopusApi.js
@@ -10,77 +10,81 @@
  * Put `import 'isomorphic-fetch';` above `export default...`.
  */
 
-const resultParser = Symbol('resultParser');
+const resultParser = Symbol("resultParser");
 
 export default class ScopusApi {
-    /**
-     * @type {string} - API URL
-     */
-    static SCOPUS_URL = 'https://api.elsevier.com/';
+  /**
+   * @type {string} - API URL
+   */
+  static SCOPUS_URL = "https://api.elsevier.com/";
 
-    static PATH_DOCUMENT_SEARCH = 'content/search/scopus';
+  static PATH_DOCUMENT_SEARCH = "content/search/scopus";
 
-    constructor(uri, apiKey = '', queryExtraParams = {}) {
+  constructor(uri, apiKey = "", queryExtraParams = {}) {
+    this.baseUrl = new URL(this.constructor.SCOPUS_URL + uri);
 
-        this.baseUrl = new URL(this.constructor.SCOPUS_URL + uri);
+    this.headers = {
+      "X-ELS-APIKey": apiKey,
+      Accept: "application/json",
+    };
 
-        this.headers = {
-            'X-ELS-APIKey': apiKey,
-            'Accept': 'application/json'
-        };
+    this.queryExtraParams = queryExtraParams;
 
-        this.queryExtraParams = queryExtraParams;
+    this.resultParser = (result) => result;
+  }
 
-        this.resultParser = result => result;
+  /**
+   * resultParser property setter
+   * @param parser {function} - new value
+   */
+  set resultParser(parser) {
+    if (typeof parser !== "function") {
+      console.warn(
+        "Incorrect result parser type. Parser should be a function."
+      );
+      return;
     }
 
-    /**
-     * resultParser property setter
-     * @param parser {function} - new value
-     */
-    set resultParser(parser) {
-        if (typeof parser !== 'function') {
-            console.warn('Incorrect result parser type. Parser should be a function.');
-            return;
-        }
+    this[resultParser] = parser;
+  }
 
-        this[resultParser] = parser;
+  /**
+   * resultParser property getter
+   * @returns {function}
+   */
+  get resultParser() {
+    return this[resultParser];
+  }
+
+  /**
+   * Request API for data
+   * @param params {json} - GET params
+   * @returns {Promise<*[]|*>}
+   */
+  async fetch(params = {}) {
+    try {
+      const result = await fetch(this.getQueryUrl(params), {
+        headers: this.headers,
+      });
+
+      const data = await result.json();
+      return this.resultParser(data);
+    } catch (e) {
+      console.warn(e);
+      return [];
     }
+  }
 
-    /**
-     * resultParser property getter
-     * @returns {function}
-     */
-    get resultParser() {
-        return this[resultParser];
-    }
-
-    /**
-     * Request API for data
-     * @param params {json} - GET params
-     * @returns {Promise<*[]|*>}
-     */
-    async fetch(params = {}) {
-        try {
-            const result = await fetch(this.getQueryUrl(params), {
-                headers: this.headers
-            });
-
-            const data = await result.json();
-            return this.resultParser(data);
-        } catch (e) {
-            console.warn(e);
-            return [];
-        }
-    }
-
-    /**
-     * Prepare query url base on API url, path and params
-     * @param params {json} - GET params
-     * @returns {string}
-     */
-    getQueryUrl(params) {
-        this.baseUrl.search = new URLSearchParams({...this.queryExtraParams, ...params}).toString();
-        return this.baseUrl.toString();
-    }
+  /**
+   * Prepare query url base on API url, path and params
+   * @param params {json} - GET params
+   * @returns {string}
+   */
+  getQueryUrl(params) {
+    this.baseUrl.search = new URLSearchParams({
+      ...this.queryExtraParams,
+      ...params,
+    }).toString();
+    return this.baseUrl.toString();
+  }
 }
--- src/lib/api/scopusApiParser.js
+++ src/lib/api/scopusApiParser.js
@@ -1,17 +1,50 @@
+import config from "../../config.json";
+
 /**
  * Find scopus article URL from list of URLs
  * @param link {{}[]} - list uf url objects
  * @returns {string}
  */
 const findScopusUrl = (link) => {
-    if (!link) {
-        return '';
-    }
+  if (!link) {
+    return "";
+  }
 
-    let linkObject = link.find(item => item['@ref'] === 'scopus');
+  let linkObject = link.find((item) => item["@ref"] === "scopus");
 
-    return linkObject?.['@href'] || '';
-}
+  return linkObject?.["@href"] || "";
+};
+
+/**
+ * Find scopus abstract URL from list of URLs
+ * @param link {{}[]} - list uf url objects
+ * @returns {string}
+ */
+const findScopusAbstractUrl = (link) => {
+  if (!link) {
+    return "";
+  }
+
+  let abstractLinkObject = link.find((item) => item["@ref"] === "self");
+
+  return abstractLinkObject?.["@href"] || "";
+};
+
+const fetchAbstractData = async (linkObject) => {
+  const abstractUrl =
+    findScopusAbstractUrl(linkObject) +
+    "?apiKey=" +
+    config.API_KEY +
+    "&httpAccept=application/json";
+  try {
+    const fetchData = await fetch(abstractUrl.toString());
+    const abstractData = await fetchData.json();
+    return abstractData?.["abstracts-retrieval-response"];
+  } catch (e) {
+    console.warn(e);
+    return [];
+  }
+};
 
 /**
  * Function parse response from Scopus search api and create structure we intend to use for feed
@@ -19,18 +52,20 @@ const findScopusUrl = (link) => {
  * @returns {[]} - list of search elements.
  */
 export default (response) => {
-    const result = [];
-    const entry = response?.['search-results']?.entry || [];
+  const result = [];
+  const entry = response?.["search-results"]?.entry || [];
 
-    entry.forEach(record => {
-        result.push({
-            id: record['dc:identifier'],
-            title: record['dc:title'],
-            author: record['dc:creator'],
-            publisher: record['prism:publicationName'],
-            url: findScopusUrl(record.link)
-        });
+  entry.forEach(async (record) => {
+    // const { affiliation, coredata } = await fetchAbstractData(record.link);
+    console.log(await fetchAbstractData(record.link));
+    result.push({
+      id: record["dc:identifier"],
+      title: record["dc:title"],
+      author: record["dc:creator"],
+      publisher: record["prism:publicationName"],
+      url: findScopusUrl(record.link),
     });
-
-    return result;
-}
+  });
+  console.log(result);
+  return result;
+};
